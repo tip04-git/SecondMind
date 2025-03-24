caabb303b680d4902c936140c5d6aa4047473a05
tip04-git <tipraveena09@gmail.com>
2025-03-24 23:56:47 +0530

    Removed files from tracking

--- main.py
+++
@@ -1,7 +0,0 @@
-# main.py
-from supervisor.supervisor import Supervisor
-
-if __name__ == "__main__":
-    supervisor = Supervisor()
-    query = input("Enter research topic: ")
-    supervisor.process(query)
